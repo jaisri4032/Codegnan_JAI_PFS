fe4215cb027a9c41341065a84dc95e3a0f257b25
jaisri4032 <jaisrikodamanchili@gmail.com>
2026-04-16 12:56:59 +0530

    OOP's concepts

--- day21.py
+++ day21.py
@@ -1,31 +1,66 @@
-import pyttsx3
+'''
+-->00P'S object oriented programming is a style of programming where we model real-world
+   thing as object that contains both data and functions()
+>>>WHY:
+   *reusable of code
+   *Scalable
 
-engine = pyttsx3.init()
+>>CLASS:
+-->class is blueprint or template that defines what kind of data
+   and behaviour of certain type of object will have
+>>>SYNTAX:
+   **class class_Name:
+      pass
+>>>EXAMPLE:
+   **class bike:
+         pass
+--------------------------------------------------------------------------
+>>>OBJECT:
+-->Instance of class or an object is a real instance created from class.
+   it is actutal thing that exists in memory while program runs
 
-def speak_tex(speak_to_text):
-    print(speak_to_text)
-    engine.say(speak_to_text)
-speak_tex("hello, iam your assistant")
-engine.runAndWait()
+class car:
+    pass
+car1 = car()#objects
+car2 = car()#objects
+-----------------------------------------------------------------------------
+>>>ATTRIBUTES:
+--> these are the variables that store data realated to class or objects
 
-import pyttsx3
+class dog:
+    def __init__(self,breed,color):
+       self.breed = breed
+       self.color = color         
+dog_1 = dog("bull dog","white/cream")
+dog_2 = dog("labrador", "black")
+print(dog_2.breed)
+'''
+class car:
+    wheels = 4
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.mileage = 20
+    def drive(self, miles):
+        self.mileage += miles
+        return f"Drove {miles} Miles. Total: {self.mileage}"
+    def info(self):
+        return f"{self.make} {self.model} {self.year}"
+car1 = car(make="ford", model="mustang", year="2008")
+car2 = car(make="toyota", model="camry", year="2023")
+print(car1.info())
+print(car2.info())
+print(car2.drive(10))
 
-engine = pyttsx3.init()
 
-def speak_text(siri):
-    engine.say(siri)
-client = input("Enter you message: ").lower()
-name = "clien"
-if "my name is" in client:
-     name = client.split("my name is")[-1].strip()
-elif "name is" in client:
-    name = client.split("name is")[-1].strip()
-if client in ["hi", "hello", "hey"]:
-    respond = "Hello! how can i help you ?"
-elif "name" in client:
-    respond = f"Hello {name}, how can i help you ?"
-else:
-    respond = "sorry , i didn't understand that"
-print(respond)
-speak_text(respond)
-engine.runAndWait()
+
+
+
+
+
+
+
+
+
+
